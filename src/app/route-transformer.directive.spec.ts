eb8afb7f568519a5ff11705139d2a83dc55abd79
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2018-10-09 14:49:11 -0700

    Adding link to export to view the IG after it is done exporting

---
+++ src/app/route-transformer.directive.spec.ts
@@ -0,0 +1,8 @@
+import { RouteTransformerDirective } from './route-transformer.directive';
+
+describe('RouteTransformerDirective', () => {
+  it('should create an instance', () => {
+    const directive = new RouteTransformerDirective();
+    expect(directive).toBeTruthy();
+  });
+});
