b4de92381d550630eb5bc3bb46f59971c853884b
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2018-10-25 07:34:20 -0700

    TRIFFHIR-67: Practitioner created for user without popup screen?

--- src/app/new-user-modal/new-user-modal.component.ts
+++ src/app/new-user-modal/new-user-modal.component.ts
@@ -4,6 +4,7 @@ import {NgbActiveModal} from '@ng-bootstrap/ng-bootstrap';
 import {Globals} from '../globals';
 import {PractitionerService} from '../services/practitioner.service';
 import {AuthService} from '../services/auth.service';
+import {Route, Router} from '@angular/router';
 
 @Component({
     selector: 'app-new-user-modal',
@@ -15,6 +16,7 @@ export class NewUserModalComponent implements OnInit {
     public message: string;
 
     constructor(
+        private router: Router,
         private activeModal: NgbActiveModal,
         public globals: Globals,
         private practitionerService: PractitionerService) {
@@ -25,11 +27,22 @@ export class NewUserModalComponent implements OnInit {
         this.practitionerService.updateMe(this.practitioner)
             .subscribe((newPractitioner) => {
                 this.activeModal.close(newPractitioner);
+                this.router.navigate(['/home']);
             }, (err) => {
                 this.message = this.globals.getErrorMessage(err);
             });
     }
 
     ngOnInit() {
+    }
+
+    public get okDisabled(): boolean {
+        return !(this.practitioner &&
+            this.practitioner.name &&
+            this.practitioner.name.length > 0 &&
+            this.practitioner.name[0].given &&
+            this.practitioner.name[0].given.length > 0 &&
+            this.practitioner.name[0].given[0] &&
+            this.practitioner.name[0].family);
     }
 }
