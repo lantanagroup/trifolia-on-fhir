1004005ce35d71e70fc348aea7e10c6952c5ec5e
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2018-11-21 08:24:34 -0800

    TRIFFHIR-39: Import value sets from excel files

--- src/app/services/import.service.ts
+++ src/app/services/import.service.ts
@@ -1,13 +1,22 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
 import {Observable} from '../../../node_modules/rxjs';
+import * as XLSX from 'xlsx';
+import {Bundle, EntryComponent, ValueSet} from '../models/stu3/fhir';
+import * as _ from 'underscore';
 
 
 export class VSACImportCriteria {
-    public resourceType = 'ValueSet';
-    public id: string;
-    public username: string;
-    public password: string;
+    resourceType = 'ValueSet';
+    id: string;
+    username: string;
+    password: string;
+}
+
+export interface IExcelConversion {
+    success: boolean;
+    message?: string;
+    bundle?: Bundle;
 }
 
 @Injectable()
@@ -36,5 +45,101 @@ export class ImportService {
                     subscriber.error(err);
                 });
         });
+    }
+
+    public convertExcelToValueSetBundle(data: Uint8Array): IExcelConversion {
+        const workbook = XLSX.read(data, { type: 'array' });
+
+        if (!workbook.SheetNames || workbook.SheetNames.length === 0) {
+            return { success: false, message: 'The excel workbook must have at least one sheet in it.' };
+        }
+
+        const firstSheetName = workbook.SheetNames[0];
+        const worksheet = workbook.Sheets[firstSheetName];
+        const sheetRows = XLSX.utils.sheet_to_json(worksheet, { header: 'A' });
+
+        const valueSetBundle = new Bundle();
+        valueSetBundle.type = 'transaction';
+        valueSetBundle.entry = [];
+
+        for (let i = 1; i < sheetRows.length; i++) {
+            const rowNum = i+1;
+            const sheetRow = sheetRows[i];
+            const valueSetId = sheetRow['A'];
+            const valueSetName = sheetRow['B'];
+            const valueSetUrl = sheetRow['C'];
+            const codeSystem = sheetRow['F'];
+            const code = sheetRow['D'];
+            const display = sheetRow['E'];
+            let foundValueSetEntry = _.find(valueSetBundle.entry, (entry) => (<ValueSet> entry.resource).url === valueSetUrl);
+
+            if (!valueSetUrl) {
+                return {success: false, message: `Row ${rowNum} does not specify a value set URL`};
+            }
+
+            if (!codeSystem) {
+                return { success: false, message: `Row ${rowNum} does not specify a code system URL` };
+            }
+
+            if (!code) {
+                return { success: false, message: `Row ${rowNum} does not specify a concept code` };
+            }
+
+            if (!display) {
+                return { success: false, message: `Row ${rowNum} does not specify a concept display` };
+            }
+
+            if (!foundValueSetEntry) {
+                foundValueSetEntry = <EntryComponent> {
+                    resource: <ValueSet> {
+                        resourceType: 'ValueSet',
+                        url: valueSetUrl,
+                        name: valueSetName,
+                        compose: {
+                            include: []
+                        }
+                    },
+                    request: {
+                        method: valueSetId ? 'PUT' : 'POST',
+                        url: valueSetId ? `ValueSet/${valueSetId}` : 'ValueSet'
+                    }
+                };
+                valueSetBundle.entry.push(foundValueSetEntry);
+
+                if (valueSetId) {
+                    foundValueSetEntry.resource.id = valueSetId;
+                }
+
+                if (!valueSetName) {
+                    return { success: false, message: `Row ${rowNum} does not specify a value set name` };
+                }
+            }
+
+            const foundValueSet = <ValueSet> foundValueSetEntry.resource;
+            let foundInclude = _.find(foundValueSet.compose.include, (include) => include.system === sheetRow['E']);
+
+            if (!foundInclude) {
+                foundInclude = {
+                    system: codeSystem,
+                    concept: []
+                };
+                foundValueSet.compose.include.push(foundInclude);
+            }
+
+            let foundConcept = _.find(foundInclude.concept, (concept) => concept.code === sheetRow['C']);
+
+            if (!foundConcept) {
+                foundConcept = {
+                    code: code,
+                    display: display
+                };
+                foundInclude.concept.push(foundConcept);
+            }
+        }
+
+        return {
+            success: true,
+            bundle: valueSetBundle
+        };
     }
 }
