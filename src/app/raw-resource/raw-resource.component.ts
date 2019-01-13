d0989bfa95542f4c002c6b9e3a3b67791686dcdb
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-01-13 13:57:07 -0600

    TRIFFHIR-110: Allow user to download JSON/XML in the "RAW" tab

--- src/app/raw-resource/raw-resource.component.ts
+++ src/app/raw-resource/raw-resource.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Resource} from '../models/stu3/fhir';
+import {FhirService} from '../services/fhir.service';
+import {saveAs} from 'file-saver';
 
 @Component({
     selector: 'app-raw-resource',
@@ -9,7 +11,26 @@ import {Resource} from '../models/stu3/fhir';
 export class RawResourceComponent implements OnInit {
     @Input() resource: Resource;
 
-    constructor() {
+    constructor(private fhirService: FhirService) {
+    }
+
+    public get baseFileName() {
+        if (this.resource  && this.resource.id) {
+            return this.resource.id;
+        }
+
+        return 'resource';
+    }
+
+    public downloadJson() {
+        const blob = new Blob([JSON.stringify(this.resource, null, '\t')], { type: 'application/json; charset=utf-8' });
+        saveAs(blob, this.baseFileName + '.json');
+    }
+
+    public downloadXml() {
+        const resourceXml = this.fhirService.serialize(this.resource);
+        const blob = new Blob([resourceXml], { type: 'text/xml; charset=utf-8' });
+        saveAs(blob, this.baseFileName + '.xml');
     }
 
     ngOnInit() {
