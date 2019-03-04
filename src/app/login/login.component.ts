b3e89f5a83b6c61e7a5ebca9dc31f4f21981c5b1
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-03-04 15:46:12 -0800

    Improving error handling within the login page

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../services/auth.service';
+import {ActivatedRoute} from '@angular/router';
+import * as _ from 'underscore';
 
 @Component({
   selector: 'app-login',
@@ -7,12 +9,36 @@ import {AuthService} from '../services/auth.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+    public loggingIn = true;
+    public errorMessage: string;
 
     constructor(
+        private route: ActivatedRoute,
         private authService: AuthService) {
     }
 
     ngOnInit() {
+        if (this.route.snapshot && this.route.snapshot.fragment) {
+            const fragmentParams = _.chain(this.route.snapshot.fragment.split('&'))
+                .filter((param) => param.indexOf('=') > 0)
+                .map((param) => {
+                    const split = param.split('=');
+                    return {
+                        key: split[0],
+                        value: split[1]
+                    };
+                })
+                .value();
+            const foundError = _.find(fragmentParams, (fragmentParam) => fragmentParam.key === 'error');
+            const foundErrorDescription = _.find(fragmentParams, (fragmentParam) => fragmentParam.key === 'error_description');
+
+            if (foundError || foundErrorDescription) {
+                this.loggingIn = false;
+                this.errorMessage = foundErrorDescription.value;
+                return;
+            }
+        }
+
         this.authService.handleAuthentication();
     }
 }
