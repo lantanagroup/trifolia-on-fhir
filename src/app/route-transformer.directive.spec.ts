97a42305a888ab62826e20734a6cc5106d294c1d
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-03-25 22:57:24 -0700

    Removing useless integration tests for components

--- src/app/route-transformer.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RouteTransformerDirective } from './route-transformer.directive';
-
-describe('RouteTransformerDirective', () => {
-  it('should create an instance', () => {
-    const directive = new RouteTransformerDirective(null, null);
-    expect(directive).toBeTruthy();
-  });
-});
