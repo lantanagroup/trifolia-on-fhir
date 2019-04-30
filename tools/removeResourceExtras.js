c21486de0c36a86963a579afd661c79f3f3481c0
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2019-04-30 09:32:29 -0700

    Removing additional extra/useless information from base profiles to reduce the time it takes for the browser to download

--- tools/removeResourceExtras.js
+++ tools/removeResourceExtras.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 
 if (process.argv.length < 3) {
-    console.log('Must specify file to remove text from resources');
-    return process.exit(1);
+  console.log('Must specify file to remove text from resources');
+  return process.exit(1);
 }
 
 const filePath = process.argv[2];
@@ -10,24 +10,72 @@ let fileContent = fs.readFileSync(filePath).toString();
 const resource = JSON.parse(fileContent);
 
 function cleanResource(resource) {
-    if (!resource) {
-        return;
-    }
+  if (!resource) {
+    return;
+  }
 
-    delete resource.text;
+  console.log(`Found resource ${resource.resourceType} with url '${resource.url}`);
 
-    if (resource.resourceType === 'StructureDefinition') {
-        delete resource.differential;
+  delete resource.extension;
+  delete resource.meta;
+  delete resource.text;
+  delete resource.publisher;
+  delete resource.differential;
+  delete resource.experimental;
+  delete resource.contact;
+
+  if (resource.resourceType === 'StructureDefinition') {
+    for (let i = 0; i < resource.snapshot.element.length; i++) {
+      const element = resource.snapshot.element[i];
+
+      delete element.comment;
+      delete element.constraint;
+      delete element.mapping;
+      delete element.isSummary;
+      delete element.alias;
+      delete element.extension;
+      delete element.base;
+      delete element.isModifier;
+
+      if (element.binding) {
+        delete element.binding.extension;
+      }
     }
+  } else if (resource.resourceType === 'CodeSystem') {
+    delete resource.copyright;
+    delete resource.caseSensitive;
+    delete resource.versionNeeded;
+    delete resource.status;
+    delete resource.jurisdiction;
+    delete resource.date;
+    delete resource.description;
+    delete resource.concept;
+
+    if (resource.concept) {
+      for (let i = 0; i < resource.concept.length; i++) {
+        delete resource.concept[i].definition;
+      }
+    }
+  } else if (resource.resourceType === 'ValueSet') {
+    delete resource.copyright;
+    delete resource.description;
+  }
 }
 
 if (resource.resourceType === 'Bundle') {
-    for (let i = 0; i < resource.entry.length; i++) {
-        const entry = resource.entry[i];
-        cleanResource(entry.resource);
+  for (let i = 0; i < resource.entry.length; i++) {
+    const entry = resource.entry[i];
+    const resourceType = entry.resource.resourceType;
+
+    if (resourceType === 'OperationDefinition' || resourceType === 'CompartmentDefinition' || resourceType === 'CapabilityStatement') {
+      resource.entry.splice(i, 1);
+      i--;
+    } else {
+      cleanResource(entry.resource);
     }
+  }
 } else if (resource.resourceType) {
-    cleanResource(resource);
+  cleanResource(resource);
 }
 
 // save
