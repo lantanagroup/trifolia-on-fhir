d794f1f5141a68f93ca121513cc901c39a296244
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2020-12-04 10:29:09 -0800

    TRIFFHIR-508: Media Resources are missing contentType on attachment

--- apps/client/src/app/import/import.component.ts
+++ apps/client/src/app/import/import.component.ts
@@ -103,7 +103,7 @@ export class ImportComponent implements OnInit {
     modalRef.componentInstance.importResource = resource;
   }
 
-  private createMedia(name: string, buffer: ArrayBuffer) {
+  private createMedia(name: string, contentType: string, buffer: ArrayBuffer) {
     const b64content = btoa(new Uint8Array(buffer)
       .reduce((data, byte) => data + String.fromCharCode(byte), ''));
 
@@ -118,6 +118,7 @@ export class ImportComponent implements OnInit {
         value: name
       }];
       media.content = {
+        contentType,
         data: b64content
       };
       return media;
@@ -132,6 +133,7 @@ export class ImportComponent implements OnInit {
         value: name
       }];
       media.content = {
+        contentType,
         data: b64content
       };
       return media;
@@ -183,7 +185,7 @@ export class ImportComponent implements OnInit {
 
             importFileModel.vsBundle = convertResults.bundle;
           } else if (importFileModel.contentType === ContentTypes.Image) {
-            importFileModel.resource = this.createMedia(file.name, result);
+            importFileModel.resource = this.createMedia(file.name, file.type, result);
           }
         } catch (ex) {
           importFileModel.message = ex.message;
