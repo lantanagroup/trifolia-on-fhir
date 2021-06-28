a53a9df7c5c69010a5e88072b961a9dcfe038192
salahlantana <85251456+salahlantana@users.noreply.github.com>
2021-06-28 13:41:27 -0400

    Server details are retrieved from config. Added appropriate error han… (#79)
    * Server details are retrieved from config. Added appropriate error handling. Needs to be tested for successful publish.
    
    * Fixing config to use correct spacing and quotation marks
    
    Co-authored-by: Sean McIlvenna <sean.mcilvenna@lantanagroup.com>

--- apps/server/src/app/models/server-config.ts
+++ apps/server/src/app/models/server-config.ts
@@ -1,6 +1,9 @@
 export interface IServerConfig {
+  // This is the hostname that the server is hosted at (i.e. localhost)
   hostname?: string;
+  // This is the port that the server is hosted at (i.e. 8080)
   port?: number;
+  // Specify where the key and cert for the SSL certificates are to host the server at https:// natively through node
   https?: {
     keyPath: string;
     certPath: string;
@@ -9,7 +12,7 @@ export interface IServerConfig {
   googleAnalyticsCode?: string;
   javaLocation?: string;
   logFileName?: string;
-  logLevel?: 'all'|'trace'|'warn'|'error';
+  logLevel?: 'all' | 'trace' | 'warn' | 'error';
   maxLogSizeKB?: number;
   publishedIgsDirectory?: string;
   enableSecurity: boolean;
@@ -18,6 +21,15 @@ export interface IServerConfig {
   bannerMessage?: string;
   maxAsyncQueueRequests: number;
   publishStatusPath?: string;
+
+  // The configuration for mail server to use to send email
+  // See nodemailer for all possible options
+  mailTransport?: any;
+  mailOptions?: {
+    from: string;
+    hostUrl: string;
+    subjectPrefix?: string;
+  };
 }
 
 export function createTestServerConfig(enableSecurity = false): IServerConfig {
