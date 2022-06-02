78d73a11f6d65179c60bd913db8bcf23e81302a6
salah.hasan <salah.hasan@lantanagroup.com>
2022-06-02 16:15:47 -0400

    TRIFFHIR-611: Added button that opens FhirCodeableConceptModalComponent instead of having it displayed on the panel

--- apps/client/src/app/fhir-edit/identifier/identifier.component.ts
+++ apps/client/src/app/fhir-edit/identifier/identifier.component.ts
@@ -4,6 +4,7 @@ import { Coding, Identifier } from '../../../../../../libs/tof-lib/src/lib/stu3/
 import { CodeableConcept } from '../../../../../../libs/tof-lib/src/lib/r4/fhir';
 import { FhirCodingModalComponent } from '../coding-modal/coding-modal.component';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { FhirCodeableConceptModalComponent } from '../codeable-concept-modal/codeable-concept-modal.component';
 
 @Component({
   selector: 'app-fhir-identifier',
@@ -19,6 +20,7 @@ export class FhirIdentifierComponent implements OnInit {
   @Input() defaultValue = {};
   @Input() tooltip: string;
   @Input() tooltipKey: string;
+  @Input() choices: string[];
   @Output() change: EventEmitter<void> = new EventEmitter<void>();
 
   public Globals = Globals;
@@ -75,9 +77,37 @@ export class FhirIdentifierComponent implements OnInit {
     }
   }
 
+  public getChoiceName() {
+    let foundChoice;
+
+    for (let i = 0; i < this.choices.length; i++) {
+      const choice = this.choices[i].substring(0, 1).toUpperCase() + this.choices[i].substring(1);
+      if (this.parentObject.hasOwnProperty(this.propertyName + choice)) {
+        foundChoice = this.choices[i];
+      }
+    }
+
+    return foundChoice;
+  }
+
+  public getChoicePropertyName() {
+    const foundChoice = this.getChoiceName();
+    if (foundChoice) {
+      return this.propertyName + foundChoice.substring(0, 1).toUpperCase() + foundChoice.substring(1);
+    }
+  }
+
   editCoding(coding: Coding) {
     const modalRef = this.modalService.open(FhirCodingModalComponent, { backdrop: 'static' });
     modalRef.componentInstance.coding = coding;
+  }
+
+  editCodeableConcept() {
+    const modalRef = this.modalService.open(FhirCodeableConceptModalComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.codeableConcept = this.identifier.type;
+    modalRef.result.then(() => {
+      this.change.emit();
+    });
   }
 
   ngOnInit() {
