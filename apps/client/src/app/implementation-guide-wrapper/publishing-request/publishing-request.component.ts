5cb55f76c06369037cb317e16032d272137427e7
salah.hasan <salah.hasan@lantanagroup.com>
2022-11-15 16:20:14 -0500

    TRIFFHIR-633: Adding newly created PublishingRequestComponent and added file in lib for PublishingRequestModel & PublishingRequestItemModel

---
+++ apps/client/src/app/implementation-guide-wrapper/publishing-request/publishing-request.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'trifolia-fhir-publishing-request',
+  templateUrl: './publishing-request.component.html',
+  styleUrls: ['./publishing-request.component.css']
+})
+export class PublishingRequestComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
