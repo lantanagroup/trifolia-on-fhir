c9d7b7786f012d753e3b65a3af1400f0c9744cf1
Sean McIlvenna <sean.mcilvenna@lantanagroup.com>
2023-01-18 15:55:21 -0800

    Loading users, groups, projects/igs Loading with _id pre-determined based on the id of the resource in the FHIR server

--- apps/tools/src/app/migrate-db.ts
+++ apps/tools/src/app/migrate-db.ts
@@ -3,7 +3,8 @@ import { IGroup, IProject, IUser } from '../../../server/src/db/models';
 import { IContactPoint, IImplementationGuide, IPractitioner } from '../../../../libs/tof-lib/src/lib/fhirInterfaces';
 import {MongoClient} from 'mongodb/lib';
 import {Db} from 'mongodb';
-import { getHumanNameDisplay, getHumanNamesDisplay } from '../../../../libs/tof-lib/src/lib/helper';
+import { getHumanNamesDisplay } from '../../../../libs/tof-lib/src/lib/helper';
+import { Group } from '../../../../libs/tof-lib/src/lib/r4/fhir';
 
 interface MigrateDbOptions {
   server: string;
@@ -30,7 +31,8 @@ export class MigrateDb extends BaseTools {
 
   public async migrate() {
     await this.migrateUsers();
-    //await this.migrateImplementationGuides();
+    await this.migrateGroups();
+    await this.migrateImplementationGuides();
   }
 
   private getPhone(telecoms: IContactPoint[]) {
@@ -45,6 +47,37 @@ export class MigrateDb extends BaseTools {
     return found ? found.value : undefined;
   }
 
+  private async migrateGroups() {
+    const resources = await this.getAllResourcesByType(this.options.server, ['Group']);
+
+    if (this.options.fhirVersion === 'r4') {
+      this.groups = resources
+        .filter((r: Group) => {
+          const userId = r.managingEntity.reference.substring(r.managingEntity.reference.lastIndexOf('/') + 1);
+          const user = this.users.find(u => u._id === userId);
+          return !!user;
+        })
+        .map((r: Group) => {
+          return <IGroup> {
+            _id: r.id,
+            name: r.name,
+            managingUserId: r.managingEntity.reference.substring(r.managingEntity.reference.lastIndexOf('/') + 1),
+            member: r.member.map(m => {
+              return m.entity.reference.substring(m.entity.reference.lastIndexOf('/') + 1)
+            })
+          };
+        });
+    } else if (this.options.fhirVersion === 'stu3') {
+      throw new Error('STU3 groups not yet supported');
+    }
+
+    console.log(`Storing ${this.groups.length} groups in the database`);
+
+    for (const group of this.groups) {
+      await this.db.collection('group').updateOne({ _id: group._id }, { $set: group }, { upsert: true });
+    }
+  }
+
   private async migrateUsers() {
     const resources = await this.getAllResourcesByType(this.options.server, ['Practitioner']);
     const userResources = resources.filter((r: IPractitioner) => {
@@ -55,6 +88,7 @@ export class MigrateDb extends BaseTools {
       const identifier = r.identifier.find(i => i.system === 'https://trifolia-fhir.lantanagroup.com' || i.system === 'https://auth0.com');
 
       return <IUser> {
+        _id: r.id,
         name: getHumanNamesDisplay(r.name),
         authId: [identifier.value],
         phone: this.getPhone(r.telecom),
@@ -62,7 +96,11 @@ export class MigrateDb extends BaseTools {
       };
     });
 
-    await this.db.collection('user').insertMany(this.users);
+    console.log(`Storing ${this.users.length} users in the database`);
+
+    for (const user of this.users) {
+      await this.db.collection('user').updateOne({ _id: user._id }, { $set: user }, { upsert: true });
+    }
   }
 
   private async migrateImplementationGuides() {
@@ -70,6 +108,7 @@ export class MigrateDb extends BaseTools {
 
     this.projects = resources.map((r: IImplementationGuide) => {
       return <IProject> {
+        _id: r.id,
         name: r.name,
         fhirVersion: this.options.fhirVersion,
         permissions: [],
@@ -77,6 +116,10 @@ export class MigrateDb extends BaseTools {
       };
     });
 
-    await this.db.collection('project').insertMany(this.projects);
+    console.log(`Storing ${this.projects.length} projects in the database`);
+
+    for (const project of this.projects) {
+      await this.db.collection('project').updateOne({ _id: project._id }, { $set: project }, { upsert: true });
+    }
   }
 }
