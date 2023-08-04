e4c51e28398e5ec312221ed0ada2c8dd565e1058
bstewartlg <114590619+bstewartlg@users.noreply.github.com>
2023-08-04 18:16:58 -0400

    Additional changes to handling non-fhir resources

--- apps/server/src/app/non-fhir-resources/types/cda-example.schema.ts
+++ apps/server/src/app/non-fhir-resources/types/cda-example.schema.ts
@@ -1,19 +1,14 @@
-import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
-import { type ICdaExample, IPermission, IProject, IProjectResourceReference, NonFhirResourceType } from "@trifolia-fhir/models";
+import { Schema, SchemaFactory } from "@nestjs/mongoose";
+import { NonFhirResourceType, type ICdaExample } from "@trifolia-fhir/models";
 import { HydratedDocument } from "mongoose";
-import { NonFhirResource, NonFhirResourceBase } from "../non-fhir-resource.schema";
+import { NonFhirResourceBase } from "../non-fhir-resource.schema";
 
 export type CdaExampleDocument = HydratedDocument<CdaExample>;
 
 @Schema()
 export class CdaExample extends NonFhirResourceBase implements ICdaExample {  //extends NonFhirResource implements ICdaExample {
     readonly type: NonFhirResourceType = NonFhirResourceType.CdaExample;
-
-    @Prop()
-    public cdaProperty: string = "just a test";
 }
 
 export const CdaExampleSchema = SchemaFactory.createForClass(CdaExample);
-//CdaExampleSchema.loadClass(CdaExample);
-
-//NonFhirResource.disciminator(CdaExampleSchema.eventNames, CdaExampleSchema);
+CdaExampleSchema.loadClass(CdaExample);
