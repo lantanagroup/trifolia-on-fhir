af0ba97c3215e60cd1afbb346b4db49f5ae982a7
bstewartlg <114590619+bstewartlg@users.noreply.github.com>
2023-12-06 14:10:20 -0500

    Fix migrations to handle initial empty database

--- apps/server/src/db-migrations/migrations/20230726235623-rename-example-collection.js
+++ apps/server/src/db-migrations/migrations/20230726235623-rename-example-collection.js
@@ -1,13 +1,25 @@
 module.exports = {
   async up(db, client) {
-    // renames the "example" collection to the new "nonFhirResource" name
-    await db.renameCollection('example', 'nonFhirResource', { dropTarget: true });
-    console.log("Renamed 'example' collection to 'nonFhirResource'");
+    
+    // renames the "example" collection (if it exists) to the new "nonFhirResource" name
+    if ((await db.listCollections({name: 'example'}).toArray()).length > 0) {
+      await db.renameCollection('example', 'nonFhirResource', { dropTarget: true });
+      console.log("Renamed 'example' collection to 'nonFhirResource'");
+    } else {
+      console.log("The 'example' collection doesn't exist, skipping renaming.");
+    }
+    
   },
 
   async down(db, client) {
-    // restores old "example" collection name from "nonFhirResource"
-    await db.renameCollection('nonFhirResource', 'example', { dropTarget: true });
-    console.log("Renamed 'nonFhirResource' collection to 'example'");
+
+    // restores old "example" collection name from "nonFhirResource" (if it exists)
+    if ((await db.listCollections({name: 'nonFhirResource'}).toArray()).length > 0) {
+      await db.renameCollection('nonFhirResource', 'example', { dropTarget: true });
+      console.log("Renamed 'nonFhirResource' collection to 'example'");
+    } else {
+      console.log("The 'nonFhirResource' collection doesn't exist, skipping renaming.");
+    }    
+    
   }
 };
